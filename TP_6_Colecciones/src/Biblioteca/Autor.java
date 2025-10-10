f9adc44a3be97539a4c2dde8b6ca01001e92cc32
MaxiCode <andresholsbach@gmail.com>
2025-10-10 02:16:20 -0300

    Se finaliza ejercicio del caso practico 2

---
+++ TP_6_Colecciones/src/Biblioteca/Autor.java
@@ -0,0 +1,69 @@
+package Biblioteca;
+
+public class Autor {
+    private String id; 
+    private String nombre; 
+    private String nacionalidad;
+
+    public Autor(String id, String nombre, String nacionalidad) {
+        setId(id);
+        setNombre(nombre);
+        setNacionalidad(nacionalidad);
+    }
+
+    public void setId(String id) {
+        try {
+            if(id == null || id.trim().isEmpty()){
+                throw new IllegalArgumentException("No se puede cargar un ID vacio");
+            }
+            this.id = id;
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    public void setNombre(String nombre) {
+        try {
+            if(id == null || id.trim().isEmpty()){
+                throw new IllegalArgumentException("No se puede cargar un nombre vacio");
+            }
+            this.nombre = nombre;
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        try {
+            if(id == null || id.trim().isEmpty()){
+                throw new IllegalArgumentException("No se puede cargar una nacionalidad vacia");
+            }
+            this.nacionalidad = nacionalidad;
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+    
+    public void mostrarInfo(){
+        System.out.println("Autor: "+getNombre()+"\nID: "+getId()+"\nNacionalidad: "+getNacionalidad());
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" + "id=" + id + ", nombre=" + nombre + ", nacionalidad=" + nacionalidad + '}';
+    }
+    
+    
+}
---
+++ TP_6_Colecciones/src/Biblioteca/Libro.java
@@ -0,0 +1,78 @@
+
+package Biblioteca;
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private int anioPublicacion;
+    private Autor autor;
+
+    public Libro(String isbn, String titulo, int anioPublicacion, Autor autor) {
+        setIsbn(isbn);
+        setTitulo(titulo);
+        setAnioPublicacion(anioPublicacion);
+        this.autor = autor;
+    }
+    
+    
+
+    public void setIsbn(String isbn) {
+        try {
+            if(isbn == null || isbn.trim().isEmpty()){
+                throw new IllegalArgumentException("No se puede cargar un ID vacio");
+            }
+            this.isbn = isbn;
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    public void setTitulo(String titulo) {
+        try {
+            if(titulo == null || titulo.trim().isEmpty()){
+                throw new IllegalArgumentException("No se puede cargar un ID vacio");
+            }
+            this.titulo = titulo;
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        int anioActual = java.time.Year.now().getValue(); // Obtiene el año actual del sistema
+        try {
+            if(anioPublicacion < 0 || anioPublicacion > anioActual){
+                throw new IllegalArgumentException("No se puede cargar un ID vacio");
+            }
+            this.anioPublicacion = anioPublicacion;
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public String getAutor() {
+        return autor.getNombre();
+    }
+    
+    public void mostrarInfo(){
+        System.out.println("Titulo: "+getTitulo()+"\nID: "+getIsbn()+"\nAutor: "+getAutor());
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" + "isbn=" + isbn + ", titulo=" + titulo + ", anioPublicacion=" + anioPublicacion + ", autor=" + autor + '}';
+    }
+    
+}
