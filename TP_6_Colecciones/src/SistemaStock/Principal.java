2c2374ca2c10c69238e5d90a908cb1d42ca1085b
MaxiCode <andresholsbach@gmail.com>
2025-10-09 22:54:43 -0300

    Se finaliza ejercicio del caso practico 1

--- TP_6_Colecciones/src/SistemaStock/Principal.java
+++ TP_6_Colecciones/src/SistemaStock/Principal.java
@@ -18,29 +18,22 @@ public class Principal {
         String categoria;
         int cantidad;
         double min, max;
-
         //Creamos los productos a agregarse
-        
         Producto p1 = new Producto("A001", "Arroz Integral", 850.50, 20, CategoriaProducto.ALIMENTOS);
         Producto p2 = new Producto("E002", "Auriculares Bluetooth", 12500.00, 15, CategoriaProducto.ELECTRONICA);
         Producto p3 = new Producto("R003", "Campera Impermeable", 9800.75, 10, CategoriaProducto.ROPA);
         Producto p4 = new Producto("H004", "Lámpara LED de escritorio", 3200.00, 25, CategoriaProducto.HOGAR);
         Producto p5 = new Producto("A005", "Galletitas sin TACC", 450.90, 50, CategoriaProducto.ALIMENTOS);
         Producto p6 = new Producto("E006", "Tablet 10 pulgadas", 68999.99, 5, CategoriaProducto.ELECTRONICA);
-        
         //Creamos la entidad depositoCentral
-        
         Inventario depositoCentral = new Inventario();
-        
         depositoCentral.agregarProducto(p1);
         depositoCentral.agregarProducto(p2);
         depositoCentral.agregarProducto(p3);
         depositoCentral.agregarProducto(p4);
         depositoCentral.agregarProducto(p5);
         depositoCentral.agregarProducto(p6);
-        
         //Listamos los productos del inventario
-        
         depositoCentral.listarProductos();
         System.out.println("");
         System.out.print("Ingrese el Id para buscar el producto: ");
@@ -76,7 +69,6 @@ public class Principal {
         System.out.println("La cantidad de articulos en stock es: "+depositoCentral.obtenerTotalStock());
         System.out.println("");
         System.out.println("El articulo con mayor stock es: "+depositoCentral.obtenerProductoConMayorStock());
-        
         System.out.print("Ingrese el precio minimo que desea filtrar: ");
         min = Double.parseDouble(input.nextLine());
         System.out.print("Ingrese el precio maximo que desea filtrar: ");
